1a1b4b9c9857e46e178797d0b016b33c03618d9e
thezmmm <1563081082@qq.com>
2023-09-19 16:12:43 +0800

    初始环境配置

---
+++ main/src/test/java/com/hust23se/carrothole/CarrotholeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust23se.carrothole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotholeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
