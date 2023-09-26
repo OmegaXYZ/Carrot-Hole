e90a0182f839cb945c0759aade193c39be2f46e9
CodeOfUsami <zhang0285@foxmail.com>
2023-09-26 15:14:21 +0800

    backup:test v-for

--- carrot-hole/src/pages/HomePage.vue
+++ carrot-hole/src/pages/HomePage.vue
@@ -7,26 +7,54 @@
 
     </div>
     <div class="full-page-box">
-        <div class="contain-box">
+        <div class="contain-box" v-for="item in article">
+            <div class="article-box">
+                <div class="article-title">{{ article.title1 }}</div>
+                <div class="article-content">{{ article.content1 }}</div>
+            </div>
+            <div class="article-box">
+                <div class="article-title">{{ article.title2 }}</div>
+                <div class="article-content">文章内容2</div>
+            </div>
+            <div class="article-box">
+                <div class="article-title">文章标题3</div>
+                <div class="article-content">文章内容3</div>
+            </div>
+            <div class="article-box">
+                <div class="article-title">文章标题4</div>
+                <div class="article-content">文章内容4</div>
+            </div>
+            <div class="article-box">
+                <div class="article-title">文章标题5</div>
+                <div class="article-content">文章内容5</div>
+            </div>
+            <div class="article-box">
+                <div class="article-title">文章标题6</div>
+                <div class="article-content">文章内容6</div>
+            </div>
+            <div class="article-box">
+                <div class="article-title">文章标题7</div>
+                <div class="article-content">文章内容7</div>
+            </div>
+            <div class="article-box">
+                <div class="article-title">文章标题8</div>
+                <div class="article-content">文章内容8</div>
+            </div>
             <div class="article-box">
                 <div class="article-title">文章标题1</div>
                 <div class="article-content">文章内容1</div>
-                <div class="article-title">文章标题2</div>
-                <div class="article-content">文章内容2</div>
-                <div class="article-title">文章标题3</div>
-                <div class="article-content">文章内容3</div>
-                <div class="article-title">文章标题4</div>
-                <div class="article-content">文章内容4</div>
-                <div class="article-title">文章标题5</div>
-                <div class="article-content">文章内容5</div>
-                <div class="article-title">文章标题6</div>
-                <div class="article-content">文章内容6</div>
-                <div class="article-title">文章标题7</div>
-                <div class="article-content">文章内容7</div>
+            </div>
+            <div class="article-box">
                 <div class="article-title">文章标题8</div>
                 <div class="article-content">文章内容8</div>
+            </div>
+            <div class="article-box">
                 <div class="article-title">文章标题9</div>
                 <div class="article-content">文章内容9</div>
+            </div>
+            <div class="article-box">
+                <div class="article-title">文章标题10</div>
+                <div class="article-content">文章内容10</div>
             </div>
         </div>
         <div class="aside-box">
@@ -38,6 +66,42 @@
 <script>
 export default {
     name: 'Homepage',
+    data() {
+        return {
+            article: [{
+                title: '文章标题1',
+                content: '文章内容1'
+            }, {
+                title: '文章标题2',
+                content: '文章内容2'
+            }, {
+                title: '文章标题3',
+                content: '文章内容3'
+            }, {
+                title: '文章标题4',
+                content: '文章内容4'
+            }, {
+                title: '文章标题5',
+                content: '文章内容5'
+            }, {
+                title: '文章标题6',
+                content: '文章内容6'
+            }, {
+                title: '文章标题7',
+                content: '文章内容7'
+            }, {
+                title: '文章标题8',
+                content: '文章内容8'
+            }, {
+                title: '文章标题9',
+                content: '文章内容9'
+            }, {
+                title: '文章标题10',
+                content: '文章内容10'
+            }
+            ]
+        }
+    },
 }
 </script>
 
@@ -46,9 +110,10 @@ export default {
     width: 100%;
     height: 100vh;
     /* 100%视窗高度 */
-    background-color:#f0f0f0 ;
+    background-color: #f0f0f0;
     /* 设置盒子背景颜色 */
 }
+
 .white {
     width: 100%;
     height: 5px;
@@ -56,28 +121,33 @@ export default {
     background-color: #F6F6F6;
     /* 设置盒子背景颜色 */
 }
-.contain-box{
+
+.contain-box {
     width: 50%;
-    margin:0 auto;
-    max-width:800px;
+    margin: 0 auto;
+    max-width: 1200px;
     height: 100%;
     /* 100%视窗高度 */
 
     background-color: #F6F6F6;
-    padding: 20px; /* 内边距 */
-    border-top: 2px solid #ccc; /* 添加上边框线 */
+    padding: 20px;
+    /* 内边距 */
+    border: 2px solid #ccc;
     /* 设置盒子背景颜色 */
     /* 放置方式：左对齐，位置合适 */
     display: flex;
+    flex-direction: column;
     justify-content: flex-start;
     align-items: flex-start;
     margin-top: 0;
 }
-.article-box{
+
+.article-box {
     width: 100%;
     height: auto;
     /* 100%视窗高度 */
     background-color: #F6F6F6;
+    border-bottom: 2px solid #ccc;
     /* 设置盒子背景颜色 */
     /* 放置方式：左对齐，位置合适 */
     display: flex;
@@ -85,35 +155,39 @@ export default {
     align-items: center;
     flex-direction: column;
 }
-.article-title{
+
+.article-title {
     width: 100%;
     height: 100px;
-    text-align: left; /* 文字左对齐 */
+    text-align: left;
+    /* 文字左对齐 */
     /* 100%视窗高度 */
     background-color: #F6F6F6;
     /* 设置盒子背景颜色 */
     /* 放置方式：左对齐，位置合适 */
-    display: flex;
+    /* display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: center; */
     font-size: 30px;
     font-weight: bold;
 }
-.article-content{
+
+.article-content {
     width: 100%;
     height: 100%;
-    text-align: left; /* 文字左对齐 */
+    text-align: left;
+    /* 文字左对齐 */
     /* 100%视窗高度 */
     background-color: #F6F6F6;
     /* 设置盒子背景颜色 */
     /* 放置方式：左对齐，位置合适 */
-    display: flex;
+    /* display: flex;
     justify-content: center;
-    align-items: center;
-    flex-direction: row;
+    align-items: center; */
     font-size: 20px;
 }
-.aside-box{
+
+.aside-box {
     width: 30%;
     height: 100%;
     /* 100%视窗高度 */
@@ -124,6 +198,7 @@ export default {
     justify-content: center;
     align-items: center;
 }
+
 .top-box {
     width: 100%;
     height: 60px;
@@ -135,14 +210,17 @@ export default {
     display: flex;
     /* justify-content: center; */
     /* align-items: center; */
-    border-bottom: 2px solid #ccc; /* 添加底部边框线 */
+    border-bottom: 2px solid #ccc;
+    /* 添加底部边框线 */
 }
+
 .top-box-title {
     /* left title blue front */
     color: #1E90FF;
     font-size: 30px;
     font-weight: bold;
 }
+
 .white-top-box-title {
     /* right title white front */
     color: #F6F6F6;
