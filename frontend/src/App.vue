71f74996acf92db00686633effe79a5310c2c2e5
CodeOfUsami <zhang0285@foxmail.com>
2023-10-24 16:32:04 +0800

    feat: import wangeditor

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,12 @@
 <template>
     <div id="app">
-      <router-view />
+        <router-view />
     </div>
-  </template>
-  
-  <script>
-  // import Dashboard from './components/Dashboard.vue';
-  export default {
+</template>
+
+<script>
+// import Dashboard from './components/Dashboard.vue';
+export default {
     name: 'app'
-  }
-  </script>
+}
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,40 +1,42 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 // import {Vue from 'vue'
-import App from './App.vue'
+import App from "./App.vue";
 // import {Router} from 'vue-router'
 
 // createApp(App).mount('#app')
 
-import Dashboard from "@/components/Dashboard.vue"
-import DashboardHome from '@/pages/Home.vue'
+import Dashboard from "@/components/Dashboard.vue";
+import DashboardHome from "@/pages/Home.vue";
 
-import store from './store/index.js'
-import '@/assets/css/tailwind.css'
+import store from "./store/index.js";
+import "@/assets/css/tailwind.css";
 
 // Vue.config.productionTip = false
 // Vue.use(Router)
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
 // 创建路由实例
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: { name: 'DashboardHome' } },
-    { path: '/dashboard', component: Dashboard, children: [
-        { path: '/', redirect: { name: 'DashboardHome' } },
-        { path: 'home', name: 'DashboardHome', component: DashboardHome }
-      ]
-    }
+    { path: "/", redirect: { name: "DashboardHome" } },
+    {
+      path: "/dashboard",
+      component: Dashboard,
+      children: [
+        { path: "/", redirect: { name: "DashboardHome" } },
+        { path: "home", name: "DashboardHome", component: DashboardHome },
+      ],
+    },
   ],
 });
 
 // 导出路由实例
 export default router;
-  
-  const app = createApp(App);
 
-  app.use(router); // 使用路由
-  app.use(store); // 使用 Vuex
-  
-  app.mount('#app'); // 挂载 Vue 3 应用到 HTML 元素
+const app = createApp(App);
 
+app.use(router); // 使用路由
+app.use(store); // 使用 Vuex
+
+app.mount("#app"); // 挂载 Vue 3 应用到 HTML 元素
