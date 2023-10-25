37bd30b1b341e98e5157c9aa7dca1eb82b17a029
thezmmm <1563081082@qq.com>
2023-10-25 14:16:25 +0800

    发表评论以及获取某个帖子的评论的接口

--- src/main/java/com/hust23se/carrothole/service/CommentService.java
+++ src/main/java/com/hust23se/carrothole/service/CommentService.java
@@ -3,6 +3,8 @@ package com.hust23se.carrothole.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.hust23se.carrothole.entity.Comment;
 
+import java.util.List;
+
 /**
  * Comment Service
  *
@@ -10,4 +12,8 @@ import com.hust23se.carrothole.entity.Comment;
  * @date 2023-10-17
  */
 public interface CommentService extends IService<Comment> {
+
+    public boolean publishComment(String postId,String userId ,String commentContent);
+
+    public List<Comment> getCommentsByPostId(String postId);
 }
