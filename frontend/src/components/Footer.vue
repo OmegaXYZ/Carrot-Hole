9e78e9358ecbd713020a164fc738d9f94d729863
SSK015 <18252677381@163.com>
2023-10-31 18:39:00 +0800

    feat: start PostPreview Box

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="w-full border-t-2 px-8 py-6 lg:flex justify-between items-center">
-      <p class="mb-2 lg:mb-0">© Copyright 2020</p>
+      <p class="mb-2 lg:mb-0">© Copyright 2023</p>
 
       <div class="flex">
-          <a href="#" class="mr-6 hover:text-gray-900">Terms of Service</a>
-          <a href="#" class="mr-6 hover:text-gray-900">Privacy Policy</a>
-          <a href="#" class="hover:text-gray-900">About Us</a>
+          <!-- <a href="#" class="mr-6 hover:text-gray-900">Terms of Service</a> -->
+          <!-- <a href="#" class="mr-6 hover:text-gray-900">Privacy Policy</a> -->
+          <a href="#" class="hover:text-gray-900">软件工程课程作业</a>
       </div>
   </div>
 </template>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -5,19 +5,19 @@
             <nav class="text-sm font-semibold mb-6" aria-label="Breadcrumb">
               <ol class="list-none p-0 inline-flex">
                 <li class="flex items-center text-blue-500">
-                  <a href="#" class="text-gray-700">Home</a>
+                  <a href="#" class="text-gray-700">主页</a>
                   <svg class="fill-current w-3 h-3 mx-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"/></svg>
                 </li>
                 <li class="flex items-center">
-                  <a href="#" class="text-gray-600">Dashboard</a>
+                  <a href="#" class="text-gray-600">浏览帖子</a>
                 </li>
               </ol>
             </nav>
             <!-- breadcrumb end -->
 
             <div class="lg:flex justify-between items-center mb-6">
-              <p class="text-2xl font-semibold mb-2 lg:mb-0">Good afternoon, Joe!</p>
-              <button class="bg-blue-500 hover:bg-blue-600 focus:outline-none rounded-lg px-6 py-2 text-white font-semibold shadow">View Logs</button>
+              <p class="text-2xl font-semibold mb-2 lg:mb-0">早上好, 洛天依小姐!</p>
+              <button class="bg-blue-500 hover:bg-blue-600 focus:outline-none rounded-lg px-6 py-2 text-white font-semibold shadow">发布主题帖</button>
             </div>
 
             <!-- <div class="flex flex-wrap -mx-3 mb-20">
@@ -79,7 +79,9 @@
             </div> -->
 
             <div class="flex flex-wrap -mx-3">
-
+              <div class="w-full lg:w-4/5 p-3">Item 1</div>
+              <div class="w-full lg:w-4/5 p-3">Item 2</div>
+<!-- 
               <div class="w-full xl:w-1/3 px-3">
                 <p class="text-xl font-semibold mb-4">Recent Sales</p>
 
@@ -94,9 +96,9 @@
                 <div class="w-full bg-white border rounded-lg p-4 mb-8 xl:mb-0">
                   <canvas id="reviews-chart" width="600" height="400"></canvas>
                 </div>
-              </div>
+              </div> -->
 
-              <div class="w-full xl:w-1/3 px-3">
+              <!-- <div class="w-full xl:w-1/3 px-3">
                 <p class="text-xl font-semibold mb-4">Recent Transactions</p>
                 <div class="w-full bg-white border rounded-lg p-4">
                   <div class="w-full bg-gray-100 border rounded-lg flex justify-between items-center px-4 py-2 mb-4">
@@ -131,7 +133,7 @@
                       <span class="text-green-500 font-semibold text-lg">$44.99</span>
                   </div>
                 </div>
-              </div>
+              </div> -->
 
             </div>
 
