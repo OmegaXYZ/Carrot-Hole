c8c627b92535ede095cd6552340b1be6e9f23709
CodeOfUsami <zhang0285@foxmail.com>
2023-11-03 15:16:06 +0800

    增加request.js

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -2,8 +2,12 @@ import MyAxios from "./axios";
 
 export const getPostByIdAPI = async (postId) => {
     return MyAxios({
-        url:`/api/post/${postId}`,
-        method:'get',
+        url:`/api/post`,
+        method:'post',
+        data:{ID:`${postId}`},
+        params:{interfaceState:state}
+    }).then(res => {
+        console.log(res)
     });
 };
 
