15da542ab5fbaeb14338dbfecc670be55a1318be
SSK015 <18252677381@163.com>
2023-11-04 11:05:31 +0800

    feat: finish FullPoster1

--- frontend/src/components/PreviewPoster.vue
+++ frontend/src/components/PreviewPoster.vue
@@ -8,10 +8,10 @@
             </p>
         </div>
         <div class="border-t-2 border-cyan-600 my-1"></div>
-            <div class="rounded-md h-1/2 w-full bg-opacity-25 p-1">
-                <p class="text-2xl">昨夜朱楼梦</p>
-                <p class="text-md">光与影的对白</p>
-            </div>
+        <div class="rounded-md h-1/2 w-full bg-opacity-25 p-1">
+            <p class="text-2xl">昨夜朱楼梦</p>
+            <p class="text-md">光与影的对白</p>
+        </div>
         
     </div>
 </template>
