4f012e676fbdd6e66d9bb4b2e0e2c4f2f92d3234
CodeOfUsami <zhang0285@foxmail.com>
2023-11-04 22:32:22 +0800

    回帖功能完成

--- frontend/src/pages/test.vue
+++ frontend/src/pages/test.vue
@@ -1,35 +1,45 @@
-<template>
-    hello
-</template>
+<template>hello</template>
 <script>
-    import {getPostByIdAPI,getPostListAPI,getPostCommentListAPI} from '../utils/request.js';
-    import {postPublishAPI} from '../utils/post.js';
-    getPostByIdAPI(0).then(response => {
-        console.log('响应数据：', response.data);
-    })
-    .catch(error => {
-        console.error('请求失败：', error);
-    });
+import {
+  getPostByIdAPI,
+  getPostListAPI,
+  getPostCommentListAPI,
+} from "../utils/request.js";
+import { postPublishAPI, commentPublishAPI } from "../utils/post.js";
+// getPostByIdAPI(0).then(response => {
+//     console.log('响应数据：', response.data);
+// })
+// .catch(error => {
+//     console.error('请求失败：', error);
+// });
 
-    getPostListAPI().then(response => {
-        console.log('响应数据：', response.data);
-    })
-    .catch(error => {
-        console.error('请求失败：', error);
-    });
+// getPostListAPI().then(response => {
+//     console.log('响应数据：', response.data);
+// })
+// .catch(error => {
+//     console.error('请求失败：', error);
+// });
 
-    getPostCommentListAPI(0).then(response => {
-        console.log('响应数据：', response.data);
-    })
-    .catch(error => {
-        console.error('请求失败：', error);
-    });
+// getPostCommentListAPI(0).then(response => {
+//     console.log('响应数据：', response.data);
+// })
+// .catch(error => {
+//     console.error('请求失败：', error);
+// });
 
-    postPublishAPI(0, "title", "content").then(response => {
-        console.log('响应数据：', response.data);
-    })
-    .catch(error => {
-        console.error('请求失败：', error);
-    });
+postPublishAPI(0, "title", "content")
+  .then((response) => {
+    console.log("响应数据：", response.data);
+  })
+  .catch((error) => {
+    console.error("请求失败：", error);
+  });
 
+commentPublishAPI(0, 0, "commentTest1")
+  .then((response) => {
+    console.log("响应数据：", response.data);
+  })
+  .catch((error) => {
+    console.error("请求失败：", error);
+  });
 </script>
--- frontend/src/utils/post.js
+++ frontend/src/utils/post.js
@@ -1,22 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
-const postPublishUrl = 'http://localhost:8080/post/publish';
+const postPublishUrl = "http://localhost:8080/post/publish";
+const commentPublishUrl = "http://localhost:8080/comment/publish";
+
 const headers = {
-    "Content-Type": "application/json",
-  };
+  "Content-Type": "application/json",
+};
 
+export function postPublishAPI(postTitle, postContent, userId) {
+  return axios.post(
+    postPublishUrl,
+    {
+      postTitle: postTitle,
+      postContent: postContent,
+      userId: userId,
+    },
+    { headers }
+  );
+}
 
-export function postPublishAPI(postTitle,postContent,userId) {
-    return axios.post(
-        postPublishUrl,
-      {
-        postTitle: postTitle,
-        postContent:postContent,
-        userId:userId,
-      },
-      { headers }
-    );
-  }
+export function commentPublishAPI(postId, userId, commentContent) {
+  return axios.post(
+    commentPublishUrl,
+    {
+      postId: postId,
+      userId: userId,
+      commentContent: commentContent,
+    },
+    { headers }
+  );
+}
 
-
-export default {postPublishAPI};
+export default { postPublishAPI, commentPublishAPI };
