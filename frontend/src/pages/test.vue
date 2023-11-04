6379d60479575dbfa7b83321fc9a9f33543a4bd2
CodeOfUsami <zhang0285@foxmail.com>
2023-11-04 15:01:57 +0800

    bug:cros错误

--- frontend/src/pages/test.vue
+++ frontend/src/pages/test.vue
@@ -5,13 +5,14 @@
     import {helloworld,getPostByIdAPI} from '../utils/request.js';
     helloworld();
     import axios from 'axios';
-    // axios.get({
-    //     url:'http://localhost:8080/post/get',
-    //     params: {  //body参数
-    //     id:1
-    //     },
-    // }).then(res=>{
-    //     console.log(res.data);
-    // })
+    const apiUrl = 'http://localhost:8080/post/get';
+    const requestBody ={
+        postId:1
+    };
+    axios.post(apiUrl,requestBody).then((res)=>{
+        console.log(res);
+    }).catch(err=>{
+        console.log(err);
+    })
     //getPostByIdAPI(1);
 </script>
